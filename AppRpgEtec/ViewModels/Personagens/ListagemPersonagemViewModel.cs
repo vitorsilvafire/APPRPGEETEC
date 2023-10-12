608b4a73089162584a94a1202a91fa3f2877d5a3
vitor silva <vitorsilvadeoliveira050@gmail.com>
2023-10-11 21:21:32 -0300

    removendo

---
+++ AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,98 @@
+﻿using AppRpgEtec.Models.Personagens;
+using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens { get; set; }
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+
+            _ = ObterPersonagens();
+
+            NovoPersonagem = new Command(async () => { await ExibirCadastroPersonagem(); });
+            RemoverpersonagemCommand =
+                new Command<Personagem>(async (Personagem p) => { await RemoverPersonagem(p); });
+        }
+        public ICommand NovoPersonagem { get; }
+
+        public ICommand RemoverpersonagemCommand { get; }
+
+        public async Task ObterPersonagens()
+        {
+            try
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "ok");
+            }
+        }
+
+        public async Task ExibirCadastroPersonagem()
+        {
+            try
+            {
+                await Shell.Current.GoToAsync("cadPersonagemView");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        private Personagem personagemSelecionado;
+
+        public Personagem PersonagemSelecionado
+        {
+            get { return personagemSelecionado; }
+            set
+            {
+                if (value != null)
+                {
+                    personagemSelecionado = value;
+
+                    Shell.Current
+                        .GoToAsync($"cadPersonagemView?pId={personagemSelecionado.Id}");
+                }
+            }
+        }
+
+        public async Task RemoverPersonagem(Personagem p)
+        {
+            try
+            {
+                if (await Application.Current.MainPage
+                        .DisplayAlert("Confirmação", $"Confirma a remoção de {p.Nome}?", "Sim", "Não"))
+                {
+                    await pService.DeletePersonagemAsync(p.Id);
+
+                    await Application.Current.MainPage.DisplayAlert("Mensagem", "Personagem removido com sucesso!", "Ok");
+
+                    _ = ObterPersonagens();
+                }
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "ok");
+            }
+        }
+    }
+}
